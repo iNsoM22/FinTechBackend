6d0080cf94942fb67e52c7570796f4fd9c64a095
asta <s.a.moizkhan988@gmail.com>
2025-04-21 21:35:47 +0500

    Added User Routes

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import sys
 from pathlib import Path
 
-
 sys.path.insert(0, str(Path.joinpath(Path.cwd().absolute(), "/schemas/")))
 sys.path.insert(0, str(Path.joinpath(Path.cwd().absolute(), "/routers/")))
+sys.path.insert(0, str(Path.joinpath(Path.cwd().absolute(), "/validations/")))
+sys.path.insert(0, str(Path.joinpath(Path.cwd().absolute(), "/utils/")))
 
 import uvicorn
 from fastapi import FastAPI
-from routers.authentication.routes import router as auth_router
+from contextlib import asynccontextmanager
+from utils.db import create_database
+from routers.authentication.auth_routes import router as auth_router
+from routers.authentication.roles_routes import router as roles_router
+from routers.authentication.users_routes import router as users_router
 
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Initialize or Connect Database
+    await create_database()
+    
+    yield
+    
+    print("Shutting Down")
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get('/')
@@ -19,13 +34,14 @@ async def greet():
     return { 
         "Project": "Fin Tech App Backend",
         "Author": "asta",
-        "Version": "1.0.1",
+        "Version": "1.0.4",
         "Description": "A Sand Box Application for Developers for Testing."
     }
 
 
-app.include_router(auth_router)
-
+app.include_router(auth_router, tags=["Authentication"])
+app.include_router(roles_router, tags=["Roles"])
+app.include_router(users_router, tags=["Users"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ routers/authentication/users_routes.py
@@ -0,0 +1,170 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from utils.db import db_dependency
+from validations.users import (
+    UserRequest,
+    UserRead,
+    UserPublicUpdateRequest
+)
+from utils.auth import (
+    bcrypt_context,
+    require_role,
+    user_dependency
+)
+from schemas.users import User
+from typing import Annotated, List
+from uuid import UUID
+from sqlalchemy.future import select
+from schemas.roles import Role
+
+router = APIRouter(prefix="/user")
+
+
+@router.post("/add", response_model=UserRead, status_code=status.HTTP_201_CREATED)
+async def create_user(user_data: UserRequest, db: db_dependency):
+    """Add a New User with Access Level 0."""
+    try:
+        stmt = select(Role).where(Role.level == 0)
+        result = await db.execute(stmt)
+        role = result.scalar_one_or_none()
+        
+        new_user = User(
+            username=user_data.username,
+            email=user_data.email,
+            password=bcrypt_context.hash(user_data.password),
+            role_id=role.id)
+
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
+        return UserRead.model_validate(new_user)
+    
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Error Creating Public User: {str(e)}")
+
+
+@router.post("/admin/add", response_model=UserRead, status_code=status.HTTP_201_CREATED)
+async def create_internal_user(user_data: UserRequest,
+                               db: db_dependency,
+                               current_user: Annotated[dict, Depends(require_role(2))]):
+    """Add a New User with Certain Access Levels (Internal Use Only)."""
+    try:
+        if not user_data.role_id in [1, 2, 3]:
+            raise 
+        
+        new_user = User(
+            username=user_data.username,
+            password=bcrypt_context.hash(user_data.password),
+            email=user_data.email,
+            role_id=user_data.role_id
+        )
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
+        return UserRead.model_validate(new_user)
+
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Error Creating Internal User: {str(e)}")
+
+
+@router.get("/get/{identifier}", response_model=UserRead, status_code=status.HTTP_200_OK)
+async def get_user_from_identifier(identifier: str | UUID,
+                                   db: db_dependency,
+                                   current_user: user_dependency,
+                                   from_username: bool = False,
+                                   from_email: bool = False):
+    """Usage and Purpose of this API End-Point will be Resolved Later."""
+    try:
+        if current_user is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail="Could Not Validate User")
+            
+        stmt = select(User)
+        if from_username:
+            stmt = stmt.where(User.username == identifier)
+        elif from_email:
+            stmt = stmt.where(User.email == identifier)
+        else:
+            stmt = stmt.where(User.id == identifier)
+        
+        result = await db.execute(stmt)
+        user = result.scalar_one_or_none()        
+        if user is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="User Not Found")
+            
+        return UserRead.model_validate(user)
+
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                            detail=f"Error Fetching User: {str(e)}")
+
+
+@router.get("/all", response_model=List[UserRead], status_code=status.HTTP_200_OK)
+async def get_all_users(db: db_dependency,
+                        current_user: Annotated[dict, Depends(require_role(2))],
+                        limit: int = 50,
+                        offset: int = 0):
+    try:
+        stmt = select(User).offset(offset).limit(limit)
+        result = await db.execute(stmt)
+        users = result.scalars().all()
+        return [UserRead.model_validate(user) for user in users]
+
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                            detail=f"Error Fetching Users Records: {str(e)}")
+
+
+@router.put("/mod", response_model=UserRead, status_code=status.HTTP_202_ACCEPTED)
+async def modify_user(updated_user: UserPublicUpdateRequest,
+                      db: db_dependency,
+                      current_user: user_dependency):
+    """Modify Details of the Current Authenticated User."""
+    try:
+        if current_user is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could Not Validate User"
+            )
+
+        stmt = select(User).where(User.id == current_user["id"], User.username == current_user["username"])
+        result = await db.execute(stmt)
+        user = result.scalar_one_or_none()
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User Not Found"
+            )
+
+        if updated_user.new_username is not None:
+            user.username = updated_user.new_username
+        if updated_user.new_password is not None:
+            user.password = bcrypt_context.hash(updated_user.new_password)
+        if updated_user.new_email is not None:
+            user.email = updated_user.new_email
+
+        await db.commit()
+        await db.refresh(user)
+        return UserRead.model_validate(user)
+
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                            detail=f"Error Modifying User: {str(e)}")
